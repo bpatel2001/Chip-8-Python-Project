b8bd2143853a9b67f9ab02806b00df3dc3f1c358
bpatel2001 <125521942+bpatel2001@users.noreply.github.com>
2024-10-29 18:05:49 -0500

    Implemented logical operations and various other functions, figuring out key mapping

--- chip8.py
+++ chip8.py
@@ -1,31 +1,77 @@
 from chip8screen import Screen
 import pygame
 import time
+import random
 
 class chip8:
-    def __init__(self, path):
+    def __init__(self, path, setvxvy):
         self.path = path
+        self.setvxvy = setvxvy
         self.memory = [0] * 4096
         self.v = [0] * 16
         self.PC = 0x200
+        self.stack = []
         self.I = 0
         self.allocatefont()
+        self.keypad = {1: 0x1, 2: 0x2, 3: 0x3, 4: 0xC,
+                       'q': 0x4, 'w': 0x5, 'e': 0x6, 'r': 0xD,
+                        'a': 0x7, 's': 0x8, 'd': 0x9, 'f': 0xE,
+                        'z': 0xA, 'x': 0x0, 'c': 0xB, 'v': 0xF,
+                    }
 
         self.run = True
-
+        self.key = 0
         self.display = Screen()
 
+    # def keypressed(self):
+    #     key = pygame.key.get_pressed()
+    #     if key[pygame.K_1] == True:
+    #         return 0x1
+    #     elif key[pygame.K_2] == True:
+    #         return 0x2
+    #     elif key[pygame.K_3]== True:
+    #         return 0x3
+    #     elif key[pygame.K_4] == True:
+    #         return 0xC
+    #     elif key[pygame.K_q] == True:
+    #         return 0x4
+    #     elif key[pygame.K_w] == True:
+    #         return 0x5
+    #     elif key[pygame.K_e] == True:
+    #         return 0x6
+    #     elif key[pygame.K_r] == True:
+    #         return 0xD
+    #     elif key[pygame.K_a] == True:
+    #         return 0x7
+    #     elif key[pygame.K_s] == True:
+    #         return 0x8
+    #     elif key[pygame.K_d] == True:
+    #         return 0x9
+    #     elif key[pygame.K_f] == True:
+    #         return 0xE
+    #     elif key[pygame.K_z] == True:
+    #         return 0xA
+    #     elif key[pygame.K_x] == True:
+    #         return 0x0
+    #     elif key[pygame.K_c] == True:
+    #         return 0xB
+    #     elif key[pygame.K_v] == True:
+    #         return 0xF
+    #     else:
+    #         return 0
+        
     def startgame(self):
         while self.run:
             self.loadrom()
+            #self.keypressed()
             self.executeopcode()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.run = False
             pygame.display.update()
-            time.sleep(1/60)
+            time.sleep(1/500)
         pygame.quit()
-
+    
     def allocatefont(self):
         font = [0xF0, 0x90, 0x90, 0x90, 0xF0,
                 0x20, 0x60, 0x20, 0x20, 0x70,
@@ -64,9 +110,9 @@ class chip8:
     def executeopcode(self):
         opcode = self.fetchopcode()
         n1 = opcode & 0xF000
-        n2 = opcode & 0x0F00
-        n3 = opcode & 0x00F0
-        n4 = opcode & 0x000F
+        n2 = opcode & 0xF00
+        n3 = opcode & 0xF0
+        n4 = opcode & 0xF
 
         #Clear display (00E0)
         if opcode == 0x00E0:
@@ -75,10 +121,50 @@ class chip8:
                     self.display.pixel_array[Y,X] = (0,0,0)
             pygame.display.flip()
         
+        #Return subroutine from top of stack (00EE)
+        elif opcode == 0x00EE:
+            self.PC = self.stack.pop()
+        
         #Make PC jump to NNN position (1NNN)
         elif n1 == 0x1000:
             jump_pos = n2 + n3 + n4
             self.PC = jump_pos
+
+        #Append subroutine to stack and PC jumps to NNN position (2NNN)
+        elif n1 == 0x2000:
+            self.stack.append(self.PC)
+            jump_pos = n2 + n3 + n4
+            self.PC = jump_pos
+
+        #Skip one instruction if register value at x position is equal to NN (3XNN)
+        elif n1 == 0x3000:
+            value = n3 + n4
+            pos = n2 >> 8
+            if self.v[pos] == value:
+                self.PC += 2
+        
+        #Skip one instruction if register value at x position is not equal to NN (4XNN)
+        elif n1 == 0x4000:
+            value = n3 + n4
+            pos = n2 >> 8
+            if not self.v[pos] == value:
+                self.PC += 2
+
+        #Skip one instruction if register value at x position is equal to register value at y position (5XY0)
+        elif n1 == 0x5000:
+            x_pos = n2 >> 8
+            y_pos = n3 >> 4
+
+            if self.v[x_pos] == self.v[y_pos]:
+                self.PC += 2
+        
+        #Skip one instruction if register value at x position is not equal to register value at y position (9XY0)
+        elif n1 == 0x9000:
+            x_pos = n2 >> 8
+            y_pos = n3 >> 4
+
+            if not self.v[x_pos] == self.v[y_pos]:
+                self.PC += 2
 
         #Change register value at x position (6XNN)
         elif n1 == 0x6000:
@@ -90,12 +176,92 @@ class chip8:
         elif n1 == 0x7000:
             value = n3 + n4
             pos = n2 >> 8
-            self.v[pos] += value
+            self.v[pos] = (self.v[pos] + value) & 0xFF
 
-        #Set index register I (AXNNN)
+        #Logical Operations (8XYN)
+        elif n1 == 0x8000:
+            x_pos = n2 >> 8
+            y_pos = n3 >> 4
+
+            #Set operation (8XY1)
+            if n4 == 0:
+                self.v[x_pos] = self.v[y_pos]
+                
+            #Binary OR operation (8XY1)    
+            elif n4 == 1:
+                self.v[x_pos] = self.v[x_pos] | self.v[y_pos]
+            
+            #Binary AND operation (8XY2)
+            elif n4 == 2:
+                self.v[x_pos] = self.v[x_pos] & self.v[y_pos]
+
+            #Binary XOR operation (8XY3)
+            elif n4 == 3:
+                self.v[x_pos] = self.v[x_pos] ^ self.v[y_pos]
+
+            #Add operation (8XY4)
+            elif n4 == 4:
+                self.v[x_pos] = self.v[x_pos] + self.v[y_pos] & 0xFF
+
+                if self.v[x_pos] + self.v[y_pos] > 0xFF:
+                    self.v[0xF] = 1
+                else:
+                    self.v[0xF] = 0
+
+            #Subtract operation X-Y (8XY5)
+            elif n4 == 5:
+                self.v[x_pos] = self.v[x_pos] - self.v[y_pos] & 0xFF
+
+                if self.v[x_pos] > self.v[y_pos]:
+                    self.v[0xF] = 1
+                else:
+                    self.v[0xF] = 0
+
+            #Subtract operation Y-X (8XY7)
+            elif n4 == 7:
+                self.v[x_pos] = self.v[y_pos] - self.v[x_pos] & 0xFF
+
+                if self.v[y_pos] > self.v[x_pos]:
+                    self.v[0xF] = 1
+                else:
+                    self.v[0xF] = 0
+
+            #Bitwise right shift operation (8XY6) 
+            elif n4 == 6:
+                if self.setvxvy == True:
+                    self.v[x_pos] = self.v[y_pos]
+
+                self.v[0xF] = self.v[x_pos] & 1
+
+                self.v[x_pos] = self.v[x_pos] >> 1 & 0xFF
+
+            #Bitwise left shift operation (8XYE)
+            elif n4 == 0xE:
+                if self.setvxvy == True:
+                    self.v[x_pos] = self.v[y_pos]
+
+                self.v[0xF] = (self.v[x_pos] >> 7) & 1
+                    
+                self.v[x_pos] = self.v[x_pos] << 1 & 0xFF
+                
+        #Set index register I (ANNN)
         elif n1 == 0xA000:
             value = n2 + n3 + n4
             self.I = value
+
+        #Jump with offset (BNNN)
+        elif n1 == 0xB000:
+            self.PC = n2 + n3 + n4 + self.v[0]
+
+        #Generate random number (CXNN)
+        elif n1 == 0xC000:
+            pos = n2 >> 8
+            self.v[pos] = random.randint(0,255) & (n3 + n4)
+
+        elif n1 == 0xE:
+            if n3 == 0x90:
+                key = pygame.key.get_pressed()
+
 
         #Draw instruction (DXYN)
         elif n1 == 0xD000:
@@ -108,7 +274,6 @@ class chip8:
 
             for Y in range(spriteheight):
                 nthspritebyte = self.memory[self.I + Y]
-                print(nthspritebyte)
                 if ycoordinate < self.display.SCREEN_HEIGHT:
                     for X in range(8):
                             bit = (nthspritebyte >> (7 - X)) & 0x01
@@ -124,5 +289,5 @@ class chip8:
             pygame.display.flip()
 
 
-c8 = chip8("E:/CHIP-8/Chip-8-Python-Project/roms/IBM Logo.ch8")
+c8 = chip8("E:/CHIP-8/Chip-8-Python-Project/roms/test_opcode.ch8", False)
 c8.startgame()
--- chip8screen.py
+++ chip8screen.py
@@ -4,8 +4,10 @@ class Screen:
     def __init__(self):
         self.SCREEN_WIDTH = 64
         self.SCREEN_HEIGHT = 32
-        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT), flags = pygame.SCALED | pygame.RESIZABLE)
         self.pixel_array = pygame.PixelArray(self.screen)
+
 
         pygame.init()
 
