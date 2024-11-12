2f41b9e887c201b5027fff510df3b8d5eb23e369
bpatel2001 <125521942+bpatel2001@users.noreply.github.com>
2024-11-11 23:58:21 -0600

    gif

--- chip8screen.py
+++ chip8screen.py
@@ -5,7 +5,7 @@ class Screen:
         self.SCREEN_WIDTH = 64
         self.SCREEN_HEIGHT = 32
 
-        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT), flags = pygame.SCALED | pygame.RESIZABLE)
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT), flags = pygame.SCALED | pygame.RESIZABLE | pygame.FULLSCREEN)
         self.pixel_array = pygame.PixelArray(self.screen)
 
 
